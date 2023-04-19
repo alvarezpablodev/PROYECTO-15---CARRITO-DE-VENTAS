14f852ee811cd65df81250904126b9e315faf62c
Pablo Alvarez <pablo.alvarez.dev@gmail.com>
2023-04-19 10:48:55 -0400

    rama:pablo funciones agregadas : articulos carrito , limpio y agregados al contenedor

--- js/app.js
+++ js/app.js
@@ -1,24 +1,75 @@
-//variables
-
-// const carrito = document.querySelector('');
-// const vaciarCarritoBtn = document.querySelector('');
-// const agregarCarrito = document.querySelector('');
+//Variables
+const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
+
+
+
 
 
 
 //cargarEventos
 
-listaCursos.addEventListener('click',AgregarCurso);
+listaCursos.addEventListener('click',agregarCurso);
 
 
 //Funciones
-
-//Funcion AgregarCurso
-function AgregarCurso(e){
+function agregarCurso(e) {
     e.preventDefault();
+    // Delegation para agregar-carrito
     if (e.target.classList.contains('agregar-carrito')) {
-        console.log('aaa');
-        
+         const cursoSeleccionado = e.target.parentElement.parentElement;
+         // Enviamos el curso seleccionado para tomar sus datos
+         leerDatosCurso(cursoSeleccionado);
     }
+}
+
+//leer los datos del curso
+function leerDatosCurso(curso) {
+    const infoCurso = {
+         imagen: curso.querySelector('img').src,
+         titulo: curso.querySelector('h4').textContent,
+         precio: curso.querySelector('.precio span').textContent,
+         id: curso.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
+    }
+    articulosCarrito = [...articulosCarrito,infoCurso];
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+
+
+function carritoHTML() {
+
+     vaciarCarrito();
+
+     articulosCarrito.forEach(curso => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+               <td>
+                    <img src="${curso.imagen}" width = 100px> 
+               </td>
+               <td> ${curso.titulo} </td>
+               <td> ${curso.precio} </td>
+               <td> ${curso.cantidad} </td>
+               <td> <a href = "#" class="borrar-curso" data-id= ${curso.id} > X </a> </td>
+
+          `;
+          contenedorCarrito.appendChild(row);
+     });
+
+}
+
+
+
+
+function vaciarCarrito(){
+    
+    while(contenedorCarrito.firstChild) {
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+      }
 }
